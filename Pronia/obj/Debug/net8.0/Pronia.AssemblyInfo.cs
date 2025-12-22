9cdadc4b65e053a7dd91fed1879e451889de0e07
fatimajavadzada <fatimaej-mpa201@code.edu.az>
2025-12-23 01:09:17 +0400

    slider and service creat views ui fixed

--- Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
+++ Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bf1f80ed764963da752a3dd1cfda455eea142d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e5c6a55a51298fc4f79e4c31aab454c66631b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
