aa8aaeb17efe5939ac838122dc00135c8f0e0c41
fatimajavadzada <fatimaej-mpa201@code.edu.az>
2025-12-24 23:18:00 +0400

    Category CRUD added

---
+++ Pronia/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pronia.Contexts;
+using Pronia.Models;
+
+namespace Pronia.Areas.Admin.Controllers;
+[Area("Admin")]
+public class CategoryController(AppDbContext _context) : Controller
+{
+    public IActionResult Index()
+    {
+        var categories = _context.Categories.ToList();
+        return View(categories);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category category)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(category);
+        }
+
+        _context.Categories.Add(category);
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public IActionResult Update(int id)
+    {
+        var category = _context.Categories.Find(id);
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Update(Category category)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(category);
+        }
+
+        var existCategory = _context.Categories.Find(category.Id);
+
+        if (existCategory is null)
+        {
+            return NotFound();
+        }
+
+        existCategory.Name = category.Name;
+       
+
+        _context.Categories.Update(existCategory);
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Delete(int id)
+    {
+        var category = _context.Categories.Find(id);
+
+        if (category is null)
+        {
+            return NotFound();
+        }
+
+        _context.Categories.Remove(category);
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+}
--- Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
+++ Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+968ac7bdd72eec30ff7dfcc88e54587c8b8e3682")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f6eb034cd5945cc13244e4f8fd1e8e6c86a7618")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
